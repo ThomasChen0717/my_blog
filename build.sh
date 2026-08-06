f75603355b2ff824d4da644eafe240bd2910d1c0
ThomasChen0717 <thomaschenperiod3@gmail.com>
2026-08-06 10:11:09 +0800

    8/6 Commit

--- build.sh
+++ build.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 # Render build script
+# Build Command: ./build.sh
 set -o errexit
 
+pip install --upgrade pip
 pip install -r requirements.txt
 
 python manage.py collectstatic --noinput
 python manage.py migrate
 python manage.py compilemessages -l en
+python manage.py createsuperuser --noinput || true
